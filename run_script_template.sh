7d63fb5d33657941d113ee1f72ba36171c2a9f9a
Em Grasmeder <egrasmed@thoughtworks.com>
2019-07-24 15:11:15 +0200

    Happy with the run script template

--- run_script_template.sh
+++ run_script_template.sh
@@ -18,10 +18,24 @@ run_something_else() {
   # do other stuff
 }
 
+run_failure() {
+  echo "I'm about to fail!"
+  echo "This should be the last line of output"
+  curl aslkdj
+}
+
+validate_args() {
+  acceptable_args="$(declare -F | sed -n "s/declare -f run_//p" | tr '\n' ' ')"
+  if [[ ! " $acceptable_args " =~ .*\ $1\ .* ]]; then
+      echo "Invalid argument: $1"
+      echo -e "Available commands:\n$(declare -F | sed -n "s/declare -f run_/ - /p")"
+      exit 1
+  fi
+}
+
 CMD=${1:-}
 shift || true
-if run_${CMD}; then
+if validate_args ${CMD}; then
+  echo "Argument was valid, but I've got nothing to do!"
   exit 0
-else 
-  echo -e "Available commands:\n$(declare -F | sed -n "s/declare -f run_/ - /p")"
-fi 2>/dev/null
+fi
